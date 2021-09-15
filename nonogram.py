4f0b7dad5494fb83c8bb1d5574b26b75626a16d4
Hubert Życiński <hzycinski@gmail.com>
2021-09-15 21:54:18 +0200

    Better board with colors changing on click

--- nonogram.py
+++ nonogram.py
@@ -1,17 +1,19 @@
 import pygame
 import sys
 
-# Screen size
+# Screen and block size
 SCREEN_TITLE = "Nonogram"
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
-BLOCK_WIDTH = 50
-BLOCK_HEIGHT = 50
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
+BLOCK_SIZE = 20
 
 
 # Colors
-WHITE_COLOR = (255, 255, 255)
-BLACK_COLOR = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+LIGHT_GREY = (220, 220, 220)
+GREY = (190, 190, 190)
+DARK_GREY = (119, 119, 119)
 
 # Set up the clock
 clock = pygame.time.Clock()
@@ -21,34 +23,39 @@ pygame.font.init()
 font = pygame.font.SysFont("comicsans", 75)
 
 class Game:
-    TICK_RATE = 60
 
-    tile_map = [[1, 1, 1, 1], [1, 0, 0, 0], [1, 0, 1, 0], [1, 0, 0, 0]]
+    TICK_RATE = 30
 
-
-    def __init__ (self, title, width, height):
+    def __init__ (self, title, width, height, blocks_horizontally, blocks_vertically):
         self.title = title
         self.width = width
         self.height = height
+        self.blocks_horizontally = blocks_horizontally
+        self.blocks_vertically = blocks_vertically
 
         # Set up the display
         self.game_screen = pygame.display.set_mode((width, height))
-        self.game_screen.fill(WHITE_COLOR)
+        
+
+        
         pygame.display.set_caption(title)
 
-    def get_tile_color(self, tile_contents):
-        if tile_contents == 0:
-            tile_color = WHITE_COLOR
-        if tile_contents == 1:
-            tile_color = BLACK_COLOR
-        return tile_color
+     
+    def create_blank_board(self):
+        """Creates a blank board
 
-      
-    def draw_map(self, game_screen, tile_map):
-        for j, tile in enumerate(tile_map):
-            for i, tile_contents in enumerate(tile):
-                myrect = pygame.Rect(i*BLOCK_WIDTH+100, j*BLOCK_HEIGHT+100, BLOCK_WIDTH, BLOCK_HEIGHT)
-                pygame.draw.rect(game_screen, self.get_tile_color(tile_contents), myrect)
+        Returns:
+            list: list of all white squares
+        """
+        
+        all_squares = []
+        for y in range(200, self.blocks_vertically * BLOCK_SIZE + 200, BLOCK_SIZE):
+            row = []
+            for x in range(200, self.blocks_horizontally * BLOCK_SIZE + 200, BLOCK_SIZE):
+                rect = pygame.Rect(x, y, BLOCK_SIZE-1, BLOCK_SIZE-1)
+                row.append([rect, WHITE])            
+            all_squares.append(row)
+        return all_squares
         
     def run_game_loop(self):
         is_game_over = False
@@ -63,29 +70,38 @@ class Game:
                 if event.type == pygame.QUIT:
                     is_game_over = True
 
-                # Detect key press
-                elif event.type == pygame.KEYDOWN:
-
-                    # Set direction to up when pressing "w"
-                    if event.key == pygame.K_w:
-                        direction = 1
-
-                    # Set direction to down when pressing "w"
-                    elif event.key == pygame.K_s:
-                        direction = -1
-
-
-                # Stop movement when key released
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_w or event.key == pygame.K_s:
-                        direction = 0
+                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    # check which rect was clicked and change its color on list
+                    for row in all_squares:
+                        for item in row:
+                            rect, color = item
+                            if rect.collidepoint(event.pos):
+                                if color == WHITE:
+                                    item[1] = BLACK
+                                else:
+                                    item[1] = WHITE
 
 
-               
+                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+                    # check which rect was clicked and change its color on list
+                    for row in all_squares:
+                        for item in row:
+                            rect, color = item
+                            if rect.collidepoint(event.pos):
+                                if color == WHITE:
+                                    item[1] = LIGHT_GREY
+                                else:
+                                    item[1] = WHITE
+                              
 
-            # Redraw blank screen
-            self.game_screen.fill(WHITE_COLOR)
-            self.draw_map(self.game_screen, self.tile_map)
+            # Redraw screen
+            self.game_screen.fill(WHITE)
+            pygame.draw.rect(self.game_screen, DARK_GREY, pygame.Rect(199, 199, self.blocks_horizontally * BLOCK_SIZE + 1, self.blocks_vertically * BLOCK_SIZE + 1))
+            
+            for row in all_squares:
+                for item in row:
+                    rect, color = item
+                    pygame.draw.rect(self.game_screen, color, rect)
             
           
 
@@ -95,23 +111,10 @@ class Game:
             pygame.display.update()
             clock.tick(self.TICK_RATE)
 
-
-
-
-
-
-
 pygame.init()
-
-new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
+new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT, 30, 30)
+all_squares = new_game.create_blank_board()
 new_game.run_game_loop()
-
-
-
-
-
-
-
 
 # Quit pygame and program
 pygame.quit()
